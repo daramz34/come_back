6a7df5411b5ec8481bd45b240a8b6038ec5d22be
Darasimi Rotimi <darasimirotimi@gmail.com>
2026-06-22 21:56:36 +0100

    feat: setup database orm models and implement read all items endponit

---
+++ main3.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, status, Depends
+from sqlalchemy.orm import Session
+from database import SessionLocal, Item
+
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+def home():
+    return{
+        "MSG" : "Welcome"
+    }
+
+
+@app.get("/items", status_code=status.HTTP_200_OK)
+def read_all_items(db:Session = Depends(get_db)):
+    all_items = db.query(Item).all()
+
+
+    return{
+        "status": "success",
+        "count": len(all_items),
+        "data": all_items
+    }
---
+++ seed.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from database import SessionLocal, Base, engine, Item, init_db
+
+
+def seed_db():
+    init_db()
+
+    db = SessionLocal()
+
+    try:
+        print("Checking if we have items already...")
+        if db.query(Item).count() == 0:
+            print("Database Empty. Inserting 5 premium campus items")
+
+
+            item1 = Item(name="Essentals calculus Textbook", description="MTH 102 TXT", price=4500.00)
+            item2 = Item(name="Lab coat", description="White chem/phy coat", price=3500.00)
+            item3 = Item(name="Scentific Calculator", description="MTH 102 TXT", price=4500.00)
+            item4 = Item(name="Hp laptop", description="MTH 102 TXT", price=4500.00)
+            item5 = Item(name="Mini Desk Fan", description="MTH 102 TXT", price=4500.00)
+
+            db.add_all([item1, item2, item3, item4, item5])
+            db.commit()
+            print("Successfully inserted 5 sample items into the database")
+        else:
+            print("DB already contains records")
+
+    except Exception as e:
+        print(f"AN error occured during seeding: {e}")
+        db.rollback()
+    finally:
+        db.close()
+
+
+if __name__ == "__main__":
+    seed_db()
