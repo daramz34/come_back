7e933764f8d572e21f376a9549a506916885b1b9
Darasimi Rotimi <darasimirotimi@gmail.com>
2026-07-13 03:47:10 +0100

    Part B just finished my schemas and i have done half of my crud.py routes remains the remaining half

---
+++ Campus_clinic_api/crud.py
@@ -0,0 +1,64 @@
+from Campus_clinic_api.schemas import AppointmentCreate,AppointmentResponse,AppointmentUpdate,UserCreate,UserResponse,TokenResponse
+from sqlalchemy.orm import Session
+from Campus_clinic_api.database import get_db
+from Campus_clinic_api.models import User, Appointment
+from passlib.context import CryptContext
+from typing import Literal
+
+
+pwd_Context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str):
+    return pwd_Context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_Context.verify(plain_password, hashed_password)
+def get_user_by_username(db: Session, username: str):
+    db_user = db.query(User).filter(User.username == username).first()
+    return db_user
+
+def authenticate_user(db: Session, username: str, password: str):
+    db_user = get_user_by_username(db, username)
+    if not db_user:
+        return None
+    if not verify_password(password, db_user.hashed_password):
+        return None
+    return db_user
+
+
+def create_user(db: Session, user: UserCreate):
+    db_user = User(**user.model_dump(exclude={"password"}),
+                   hashed_password=hash_password(user.password))
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+
+def create_appointment(db:Session, appointment: AppointmentCreate):
+    db_appointment = Appointment(**appointment.model_dump())
+    db.add(db_appointment)
+    db.commit()
+    db.refresh(db_appointment)
+    return db_appointment
+
+
+
+def get_all_appointments(db: Session, page: int=1, limit: int=10, status: Literal["pending", "confirmed", "cancelled"]= None):
+    offset = (page - 1) *limit
+    query = db.query(Appointment)
+    
+    if status:
+        query = query.filter(Appointment.status == status)
+
+    total = query.count()
+    appointments = (query.offset(offset).limit(limit).all())
+    return {
+        "total": total,
+        "page": page,
+        "limit": limit,
+        "results": appointments
+
+    }
+
--- Campus_clinic_api/schemas.py
+++ Campus_clinic_api/schemas.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, Field, EmailStr
-from typing import Literal
-
+from typing import Literal, Optional
+from datetime import datetime
 
 class UserCreate(BaseModel):
     username: str
@@ -20,4 +20,41 @@ class UserResponse(BaseModel):
 
 
 
-# remains the remainig schemas
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+class AppointmentCreate(BaseModel):
+    title: str
+    description: Optional[str] = None
+    date: datetime
+    doctor_id : int
+class AppointmentUpdate(BaseModel):
+    title: str
+    description: Optional[str] = None
+    date: datetime
+    status: Literal["pending", "confirmed", "cancelled"]
+class AppointmentResponse(BaseModel):
+    id: int
+    title: str
+    description: Optional[str] = None
+    date: datetime
+    status: Literal["pending", "confirmed", "cancelled"]
+    created_at: datetime
+    student: UserResponse
+    doctor: UserResponse
+
+    class Config:
+        from_attributes = True
+
+
+class PaginatedResponse(BaseModel):
+    total: int
+    page: int
+    limit : int
+    results: list[AppointmentResponse]
+
+    class Config:
+        from_attributes = True
+
+
