9bbc0ad1d6744c0e3bba24d25c92c2a63b91be82
Darasimi Rotimi <darasimirotimi@gmail.com>
2026-07-17 03:11:00 +0100

    Update to day18 adding config '.env'

--- error_project/security.py
+++ error_project/security.py
@@ -1,11 +1,15 @@
 from passlib.context import CryptContext
 from datetime import datetime, timedelta, timezone
 import jwt
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 pwd_context = CryptContext(schemes=["bcrypt"],deprecated="auto")
-SECRET_KEY = "SUPER_secret_ITEM_key"
-ALGORITHM = "HS256"
-ACCESS_TOEKN_EXPIRE_MINUTES = 30
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOEKN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
