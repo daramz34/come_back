52dafa6b34a85549c11f9a11c27d44ded3c8036a
Rotimi Oluwadarasimi <darasimirotimi@gmail.com>
2026-08-28 03:48:29 +0100

    Redo on docker

--- error_project/security.py
+++ error_project/security.py
@@ -9,7 +9,7 @@ load_dotenv()
 pwd_context = CryptContext(schemes=["bcrypt"],deprecated="auto")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOEKN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
@@ -18,6 +18,6 @@ def hash_password(password:str) -> str:
 
 def create_access_token(data: dict)-> str:
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOEKN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp":expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
